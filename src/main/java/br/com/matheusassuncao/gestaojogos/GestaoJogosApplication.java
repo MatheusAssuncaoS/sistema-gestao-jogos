d69d2a32851ae823f11a5678e54674e5207690a4
Matheus Assunção <assuncaosilva.m@gmail.com>
2026-07-30 10:47:40 -0300

    chore: setup inicial Spring Boot 4.1 com PostgreSQL e Flyway

---
+++ src/main/java/br/com/matheusassuncao/gestaojogos/GestaoJogosApplication.java
@@ -0,0 +1,13 @@
+package br.com.matheusassuncao.gestaojogos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoJogosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoJogosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/matheusassuncao/gestaojogos/GestaoJogosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.matheusassuncao.gestaojogos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoJogosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
